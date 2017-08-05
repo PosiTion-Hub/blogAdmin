9a8bee2e3f19ac3fa1375e66399c8b354cdee24e
shzhdong <342348407@qq.com>
2017-08-05 21:40:24 +0800

    add Header user

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -4,7 +4,7 @@
         <div class="user-info">
             <el-dropdown trigger="click" @command="handleCommand">
                 <span class="el-dropdown-link">
-                    <img class="user-logo" src="../../../static/img/img.jpg">
+                    <img class="user-logo" src="../../../static/img/tx.jpg">
                     {{username}}
                 </span>
                 <el-dropdown-menu slot="dropdown">
@@ -18,7 +18,7 @@
     export default {
         data() {
             return {
-                name: 'linxin'
+                name: 'Shisan'
             }
         },
         computed:{
